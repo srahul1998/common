5852942f27f2a37697f8537bcb4edb88d780b9d1
srahul1998 <47640288+srahul1998@users.noreply.github.com>
2020-01-06 14:43:43 +0530

    Add files via upload
    new file

---
+++ Common/src/main/java/com/common/jwtconfig/security/UserRole.java
@@ -0,0 +1,5 @@
+package com.common.jwtconfig.security;
+
+public enum UserRole {
+	USER, ADMIN;
+}
---
+++ Common/src/test/java/com/common/jwtconfig/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.jwtconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
